146a5656680a751b0170f11d590a014b80457870
Junaid Ahmed <junaid@donesol.co.uk>
2021-07-09 16:22:59 +0500

    changing the version in persistent menu

--- src/services/FacebookServices.js
+++ src/services/FacebookServices.js
@@ -9,7 +9,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 let getInitialSetup = () => {
   return new Promise((resolve, reject) => {
     try {
-      let url = `https://graph.facebook.com/v11.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`;
+      let url = `https://graph.facebook.com/v7.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`;
       //construct the message body
       let request_body = {
         get_started: {
